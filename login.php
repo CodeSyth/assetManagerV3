204f5d3ad08ede79e08d0197828ade37623bed8a
Centupli <36423761+Centupli@users.noreply.github.com>
2020-04-14 12:37:24 -0500

    Ready to check the db for the actual password.

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     if (isset($_POST['username']) &&  isset($_POST['password'])) {
         
         
-        if (!empty($_POST['userName']) && !empty($_POST['password'])) {
+        if (!empty($_POST['username']) && !empty($_POST['password'])) {
 
             $link = mysqli_connect("127.0.0.1", "root", "", "asset_management");
             if (!$link) {
@@ -15,7 +15,18 @@
                 exit;
             }
     
-            $errors_array[] = array("status" => "SUCCESS", "message" => "Success: A proper connection to MySQL was made! The my_db database is great. Host information: ". mysqli_get_host_info($link));
+            $un=$_POST['username'];
+            $pw=$_POST['password'];
+        
+            $sql = mysqli_query($link, "SELECT * FROM am_user_cred WHERE username = '"+$un+"' AND password = '"+$pw+"';");
+            $row = mysqli_num_rows($sql);
+            
+            if($row==1){
+              echo $errors_array[] = array("status" => "SUCCESS", "message" => "Login Successful");
+              exit();
+            } else {
+              echo $errors_array[] = array("status" => "FAIL", "message" => "Login Failure");
+            }
     
             echo json_encode($errors_array);
             exit;
@@ -30,10 +41,14 @@
             exit;
         } else if (empty($_POST['username']) &&  !empty($_POST['password'])) {
             // username not set
-            $errors_array[] = array("status" => "FAIL", "message" => "Username cannot be blank! 2");
+            $errors_array[] = array("status" => "FAIL", "message" => "Username cannot be blank!");
             echo json_encode($errors_array);
             exit;
-        }            
+        } else {
+            $errors_array[] = array("status" => "FAIL", "message" => "Not Caught");
+            echo json_encode($errors_array);
+            exit;
+        }           
     }
 
 ?>
