0a0e7d8e4d24435a68c1c330b71e3e5a5816093c
Centupli <36423761+Centupli@users.noreply.github.com>
2020-04-15 18:48:54 -0500

    co

--- pages/company.php
+++ pages/company.php
@@ -35,4 +35,90 @@ include './../main.php';
         exit;
     }
 
+
+    //Edit Company
+    if (isset($_POST['loadCompanyID'])){
+        $conn = mysqli_connect("127.0.0.1", "root", "", "asset_management");
+        if (!$conn) {
+            $errors_array[] = array("status" => "FAIL", "message" => "Error: Unable to connect to MySQL." . PHP_EOL);
+            $errors_array[] = array("status" => "FAIL", "message" => "Debugging errno: " . mysqli_connect_errno() . PHP_EOL);
+            $errors_array[] = array("status" => "FAIL", "message" => "Debugging error: " . mysqli_connect_error() . PHP_EOL);
+            echo json_encode($errors_array);
+            exit;
+        }
+        
+        if (!empty($_POST['loadCompanyID'])){
+            $t = $_POST['loadCompanyID'];
+            $sql = sprintf("SELECT * FROM am_company WHERE company_id = '%s';"
+                , $t
+                );
+
+            $result =$conn->query($sql);
+        } else {
+            $result =$conn->query("SELECT * FROM am_company");
+        }
+
+        $rows = array();
+        while($row = $result->fetch_assoc()) {
+            $rows[] = $row;
+        }
+       
+        echo json_encode($rows);
+        $conn->close();
+        exit;
+    }
+
+
+  
+
+    //Save Company
+    if (isset($_POST['isSave'])){
+        $errors_array = array();
+
+        if (empty($_POST['company_name']) 
+         ) {
+            $errors_array[] = array("status" => "FAIL", "message" => "Ensure all fields are entered properly.");
+            echo json_encode($errors_array);
+            exit;
+        }
+
+        $conn = mysqli_connect("127.0.0.1", "root", "", "asset_management");
+        if (!$conn) {
+            $errors_array[] = array("status" => "FAIL", "message" => "Error: Unable to connect to MySQL." . PHP_EOL);
+            $errors_array[] = array("status" => "FAIL", "message" => "Debugging errno: " . mysqli_connect_errno() . PHP_EOL);
+            $errors_array[] = array("status" => "FAIL", "message" => "Debugging error: " . mysqli_connect_error() . PHP_EOL);
+            echo json_encode($errors_array);
+            exit;
+        }
+
+        if (empty($_POST['companyID'])){
+            $companyID = getUUID($conn);
+        } else {
+            $companyID = $_POST['companyID'];
+        }
+
+        $errors_array[] = array("status" => "DATA", "companyID" => $companyID);
+
+        $sql = sprintf("INSERT INTO am_company(company_id, company_name, company_desc) VALUES('%s','%s','%s')"+
+                       "ON DUPLICATE KEY UPDATE company_name = '%s', company_desc = '%s';" 
+                     , $companyID
+                     , $_POST['companyName']
+                     , $_POST['companyDesc']
+                     , $_POST['companyName']
+                     , $_POST['companyDesc']
+                     );
+
+        if (mysqli_query($conn, $sql)) {
+            //success
+            $errors_array[] = array("status" => "SUCCESS", "message" => "Company Saved!");
+        } else {
+            $errors_array[] = array("status" => "FAIL", "message" => "Error: " . $sql . "" . mysqli_error($conn));
+        }
+
+
+        $conn->close();
+        echo json_encode($errors_array);
+        exit;
+    }
+
 ?>
